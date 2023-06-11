3c3b2f44a090e733ad6f4f6fcb3b9ff49a3bee17
twesterhout <14264576+twesterhout@users.noreply.github.com>
2023-06-11 13:05:34 +0200

    Add the templates field (try 3)

--- flake.nix
+++ flake.nix
@@ -18,58 +18,61 @@
     };
   };
 
-  outputs = inputs: inputs.flake-utils.lib.eachDefaultSystem (system:
-    with builtins;
-    let
-      pkgs = import inputs.nixpkgs { inherit system; };
+  outputs = inputs: inputs.flake-utils.lib.eachDefaultSystem
+    (system:
+      with builtins;
+      let
+        pkgs = import inputs.nixpkgs { inherit system; };
 
-      # Only consider source dirs and .cabal files as the source to our Haskell package.
-      # This allows the project to rebuild only when the source files change.
-      src = inputs.nix-filter.lib {
-        root = ./.;
-        include = [
-          "src"
-          "test"
-          (inputs.nix-filter.lib.matchExt "cabal")
-          "README.md"
-          "LICENSE"
-        ];
-      };
-
-      ghcVersion = "928";
-      haskellPackages = pkgs.haskell.packages."ghc${ghcVersion}".override {
-        overrides = self: suprt: {
-          halide-haskell-playground =
-            (self.callCabal2nix "halide-haskell-playground" src { });
+        # Only consider source dirs and .cabal files as the source to our Haskell package.
+        # This allows the project to rebuild only when the source files change.
+        src = inputs.nix-filter.lib {
+          root = ./.;
+          include = [
+            "src"
+            "test"
+            (inputs.nix-filter.lib.matchExt "cabal")
+            "README.md"
+            "LICENSE"
+          ];
         };
-      };
 
-    in
-    {
-      packages.default = haskellPackages.halide-haskell-playground;
-      devShells.default = haskellPackages.shellFor {
-        packages = ps: with ps; [
-          halide-haskell-playground
-        ];
-        withHoogle = true;
-        nativeBuildInputs = with pkgs; with haskellPackages; [
-          # Building and testing
-          cabal-install
-          # Language servers
-          haskell-language-server
-          nil
-          # Formatters
-          fourmolu
-          cabal-fmt
-          nixpkgs-fmt
-        ];
-      };
-      # The formatter to use for .nix files (but not .hs files)
-      # Allows us to run `nix fmt` to reformat nix files.
-      formatter = pkgs.nixpkgs-fmt;
-      templates.default = {
-        path = ./.;
-      };
-    }
-  );
+        ghcVersion = "928";
+        haskellPackages = pkgs.haskell.packages."ghc${ghcVersion}".override {
+          overrides = self: suprt: {
+            halide-haskell-playground =
+              (self.callCabal2nix "halide-haskell-playground" src { });
+          };
+        };
+
+      in
+      {
+        packages.default = haskellPackages.halide-haskell-playground;
+        devShells.default = haskellPackages.shellFor {
+          packages = ps: with ps; [
+            halide-haskell-playground
+          ];
+          withHoogle = true;
+          nativeBuildInputs = with pkgs; with haskellPackages; [
+            # Building and testing
+            cabal-install
+            # Language servers
+            haskell-language-server
+            nil
+            # Formatters
+            fourmolu
+            cabal-fmt
+            nixpkgs-fmt
+          ];
+        };
+        # The formatter to use for .nix files (but not .hs files)
+        # Allows us to run `nix fmt` to reformat nix files.
+        formatter = pkgs.nixpkgs-fmt;
+      })
+  // {
+    templates.default = {
+      path = ./.;
+      description = "Minimal Haskell project with halide-haskell";
+    };
+  };
 }
